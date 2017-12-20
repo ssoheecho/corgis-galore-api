fdd3f85374493a233f3348443bef2afb96070153
soheecho94 <soheecho94@gmail.com>
2017-12-20 22:55:52 +0900

    fetch json from giphy api

--- app/controllers/api/corgis_controller.rb
+++ app/controllers/api/corgis_controller.rb
@@ -1,5 +1,11 @@
-class CorgisController < ApplicationController
+class Api::CorgisController < ApplicationController
   def index
-    
+    @resp = Faraday.get 'https://api.giphy.com/v1/gifs/search' do |req|
+      req.params['api_key'] = 'ujkGOKu3RYVCxbFOnMhK41XcYq6jSLmM'
+      req.params['q'] = 'corgis'
+    end
+    body_hash = JSON.parse(@resp.body)
+    @corgis = body_hash["data"]
+    render json: @corgis
   end
 end
