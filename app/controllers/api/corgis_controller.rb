0ad6a7708724cd85f239c04690ffced59cea247e
soheecho94 <soheecho94@gmail.com>
2017-12-21 17:41:26 +0900

    change data fetch number

--- app/controllers/api/corgis_controller.rb
+++ app/controllers/api/corgis_controller.rb
@@ -3,6 +3,7 @@ class Api::CorgisController < ApplicationController
     @resp = Faraday.get 'https://api.giphy.com/v1/gifs/search' do |req|
       req.params['api_key'] = 'ujkGOKu3RYVCxbFOnMhK41XcYq6jSLmM'
       req.params['q'] = 'corgis'
+      req.params['limit'] = 10
     end
     body_hash = JSON.parse(@resp.body)
     @corgis = body_hash["data"]
