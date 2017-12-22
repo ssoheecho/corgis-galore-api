4bc2ae81057cb075376448f6cd5e88145c8f9191
soheecho94 <soheecho94@gmail.com>
2017-12-23 00:32:27 +0900

    fetch 100 sample 10 corgis

--- app/controllers/api/corgis_controller.rb
+++ app/controllers/api/corgis_controller.rb
@@ -3,10 +3,10 @@ class Api::CorgisController < ApplicationController
     @resp = Faraday.get 'https://api.giphy.com/v1/gifs/search' do |req|
       req.params['api_key'] = 'ujkGOKu3RYVCxbFOnMhK41XcYq6jSLmM'
       req.params['q'] = 'corgis'
-      req.params['limit'] = 10
+      req.params['limit'] = 100
     end
     body_hash = JSON.parse(@resp.body)
-    @corgis = body_hash["data"]
+    @corgis = body_hash["data"].sample(10)
     render json: @corgis
   end
 end
