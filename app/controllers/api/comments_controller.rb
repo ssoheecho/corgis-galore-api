c050aef7701e3470a7ff08a3cc29438cda9b89e8
soheecho94 <soheecho94@gmail.com>
2017-12-25 01:50:06 +0900

    add create comments action

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,19 @@ class Api::CommentsController < ApplicationController
     @comments = Comment.all
     render json: @comments
   end
+
+  def create
+    byebug
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment
+    else
+      render json: { errors: { message: "FAILED TO POST COMMENT"}}
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
